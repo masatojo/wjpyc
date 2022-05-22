c6e74179c8880dc7795203ecc8df43d563724595
masa <79982360+masatojo@users.noreply.github.com>
2022-05-22 09:20:51 +0900

    Update wjpyc.sol

--- wjpyc.sol
+++ wjpyc.sol
@@ -1,3 +1,7 @@
+pragma solidity ^0.6.2;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
 contract JPYC is ERC20, owned {
 
   string private _name = "JPY Coin";
